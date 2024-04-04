472447203614357a318cac22e06d564a1e958d72
Adiklafa <temesvari.adam2004@gmail.com>
2024-04-04 15:33:22 +0200

    Első próbálkozás

---
+++ proba.py
@@ -0,0 +1,9 @@
+# importing pandas as pd 
+import pandas as pd 
+
+# read an excel file and convert 
+# into a dataframe object 
+df = pd.DataFrame(pd.read_excel("C:/Users/temes/Documents/Programming/Matematikai_programcsomagok/Projekt/github_repository/Matprog/owid-covid-data.xlsx")) 
+
+# show the dataframe 
+df 
