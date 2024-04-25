0812b3706a3f5f485a1b1651a1d746e2943efe74
Adiklafa <temesvari.adam2004@gmail.com>
2024-04-25 15:10:39 +0200

    Deleted proba.py

--- proba.py
+++
@@ -1,9 +0,0 @@
-# importing pandas as pd 
-import pandas as pd 
-
-# read an excel file and convert 
-# into a dataframe object 
-df = pd.DataFrame(pd.read_excel("C:/Users/temes/Documents/Programming/Matematikai_programcsomagok/Projekt/github_repository/Matprog/owid-covid-data.xlsx")) 
-
-# show the dataframe 
-df 
